e754ccbc7203da9be78262b64b1dd2292a14b78d
shubham_2956 <pawarshubham2956@gmail.com>
2024-12-31 14:00:36 +0530

    practice ....assert statement in python

---
+++ asertion/main.py
@@ -0,0 +1,18 @@
+def add(a,b):
+    return a+b
+
+assert add(20,40) == 60 ,"it should be 60"
+
+print("hiii")
+
+
+def sub(a,b):
+    return a-b
+
+assert sub(20, 40) == -20,"it should be -20"
+
+
+print("hiiii....")
+
+
+
